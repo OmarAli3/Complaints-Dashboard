fb38175fd9e728ea0747f6522ce8eab891a2339f
Omar Ali Hassan <omar.ali3898@gmail.com>
2023-04-11 00:36:09 +0200

    Implement complaint model and service

---
+++ src/models/ComplaintModel.ts
@@ -0,0 +1,41 @@
+export class ComplaintModel {
+  complaintNumber?: string;
+  complaintDepartment?: string;
+  complaintStatus?: string;
+  complaintDate?: string;
+  // complaintTimeFrame?: string;
+  taxpayerName?: string;
+  city?: string;
+  area?: string;
+  taxpayerBranchName?: string;
+  RIN?: string;
+  incentiveRegistered?: boolean;
+  taxpayerType?: string;
+  complaintType?: string;
+  customerName?: string;
+  customerMobile?: string;
+  customerNationalID?: string;
+
+  constructor(arg: Record<string, any>) {
+    this.complaintNumber = arg.complaint_number;
+    this.complaintDepartment = arg.complaint_department?.en;
+    this.complaintStatus = arg.complaint_status?.en;
+    this.complaintDate = arg.complaint_date;
+    // this.complaintTimeFrame = arg.complaint_time_frame;
+    this.taxpayerName = arg.taxpayer_name?.en;
+    this.city = arg.city?.en;
+    this.area = arg.area?.en;
+    this.taxpayerBranchName = arg.taxpayer_branch_name?.en;
+    this.RIN = arg.RIN;
+    this.incentiveRegistered = arg.incentive_registered;
+    this.taxpayerType = arg.taxpayer_type?.en;
+    this.complaintType = arg.complaint_type?.en;
+    this.customerName = arg.customer_name?.en;
+    this.customerMobile = arg.customer_mobile;
+    this.customerNationalID = arg.customer_nationalID;
+  }
+
+  static fromArrayResponse(array: Record<string, any>[]): ComplaintModel[] {
+    return array.map((item) => new ComplaintModel(item));
+  }
+}
